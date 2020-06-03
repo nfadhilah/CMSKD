6cd30a2d024af508f4f8737aa78686e7ff828c30
Fatah Ramadhan <fatahramadhan1992@gmail.com>
2020-06-03 14:13:36 +0700

    Coba Query

--- Persistence/Repository/BendRepository.cs
+++ Persistence/Repository/BendRepository.cs
@@ -1,7 +1,10 @@
-﻿using Domain;
+﻿using Dapper;
+using Domain;
 using MicroOrm.Dapper.Repositories;
 using MicroOrm.Dapper.Repositories.SqlGenerator;
+using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
 
 namespace Persistence.Repository
 {
@@ -15,6 +18,26 @@ namespace Persistence.Repository
       IDbConnection connection, ISqlGenerator<Bend> sqlGenerator) : base(
       connection, sqlGenerator)
     {
+    }
+
+    public async Task<IEnumerable<dynamic>> GetBendNodes(int? stAktif, string kdBank)
+    {
+      var builder = new SqlBuilder();
+
+      var cmd = builder.AddTemplate(@"SELECT TOP (1)
+                           1
+                    FROM dbo.BEND d2
+                    WHERE d2.KDBANK LIKE d.KDBANK + '%'
+                          AND d2.STAKTIF = d.STAKTIF
+                ");
+
+      if (stAktif.HasValue)
+        builder.Where("d.STAKTIF = @StAktif", new { StAktif = stAktif });
+
+      if (!string.IsNullOrWhiteSpace(kdBank))
+        builder.Where("d.KDBANK LIKE @KdBank + '%'", new { KdBank = kdBank });
+
+      return await Connection.QueryAsync(cmd.RawSql, cmd.Parameters);
     }
   }
 }
