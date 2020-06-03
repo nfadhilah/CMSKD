0705751c4a7e91089c60f431541ba199a7045288
Fatah Ramadhan <fatahramadhan1992@gmail.com>
2020-06-03 14:13:36 +0700

    Coba Query

--- API/Controllers/BendController.cs
+++ API/Controllers/BendController.cs
@@ -7,12 +7,20 @@ namespace API.Controllers
   public class BendController : BaseController
   {
     [HttpGet]
-    public async Task<IActionResult> Get() =>
-      Ok(await Mediator.Send(new List.Query()));
+    public async Task<IActionResult> Get([FromQuery] List.Query query) =>
+      Ok(await Mediator.Send(query));
+    // public async Task<IActionResult> Get() =>
+    //   Ok(await Mediator.Send(new List.Query()));
 
     [HttpGet("{id}", Name = "GetBend")]
     public async Task<IActionResult> Get(string id) =>
       Ok(await Mediator.Send(new Detail.Query { KeyBend = id }));
+
+    [HttpGet("nodes")]
+    public async Task<IActionResult> GetNode([FromQuery] Node.Query query) =>
+      Ok(
+        await Mediator.Send(new Node.Query
+        { StAktif = query.StAktif, KdBank = query.KdBank }));
 
     [HttpPost]
     public async Task<IActionResult> Create(Create.Command command)
