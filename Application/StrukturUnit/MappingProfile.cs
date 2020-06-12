a4bb73aaf0e8d11581e7a6a49fd4c439b77224f1
Kholis Arifin <kholis.arif@gmail.com>
2020-06-12 11:10:47 +0700

    tambah StruUnit

---
+++ Application/StrukturUnit/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain;
+
+namespace Application.StrukturUnit
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Create.Command, StruUnit>();
+			CreateMap<Update.Command, StruUnit>();
+		}
+	}
+}
---
+++ Domain/StruUnit.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MicroOrm.Dapper.Repositories.Attributes;
+
+namespace Domain
+{
+    [Table("STRUUNIT")]
+    public class StruUnit
+    {
+        [Key]
+        public int KdLevel { get; set; }
+        [Key, Identity]
+        public long IdStruUnit { get; set; }
+        public string NmLevel { get; set; }
+        public string NumDigit { get; set; }
+    }
+}
