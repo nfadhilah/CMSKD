55131269faeec52b0bc2b4d141955c7f91a3fe58
Mono Noor Fadhilah <halilintar@gmail.com>
2020-06-15 16:00:06 +0800

    Add DTO on Bendahara Update

--- API/Controllers/BendController.cs
+++ API/Controllers/BendController.cs
@@ -9,18 +9,10 @@ namespace API.Controllers
     [HttpGet]
     public async Task<IActionResult> Get([FromQuery] List.Query query) =>
       Ok(await Mediator.Send(query));
-    // public async Task<IActionResult> Get() =>
-    //   Ok(await Mediator.Send(new List.Query()));
 
     [HttpGet("{id}", Name = "GetBend")]
     public async Task<IActionResult> Get(string id) =>
       Ok(await Mediator.Send(new Detail.Query { IdBend = id }));
-
-    [HttpGet("nodes")]
-    public async Task<IActionResult> GetNode([FromQuery] Node.Query query) =>
-      Ok(
-        await Mediator.Send(new Node.Query
-        { StAktif = query.StAktif, IdBank = query.IdBank }));
 
     [HttpPost]
     public async Task<IActionResult> Create(Create.Command command)
@@ -30,9 +22,9 @@ namespace API.Controllers
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(string id, Update.Command command)
+    public async Task<IActionResult> Update(string id, Update.DTO dto)
     {
-      command.IdBend = id;
+      var command = dto.MapDTO(new Update.Command { IdBend = id });
       return Ok(await Mediator.Send(command));
     }
 
--- Application/Bendahara/Update.cs
+++ Application/Bendahara/Update.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Application.Interfaces;
+using AutoMapper;
 using AutoWrapper.Wrappers;
 using FluentValidation;
 using MediatR;
@@ -10,36 +11,69 @@ using System.Threading.Tasks;
 
 namespace Application.Bendahara
 {
+
   public class Update
   {
-    public class Command : IRequest
+    public class DTO : IMapDTO<Command>
     {
-    public string IdBend { get; set; }
-    public string IdUnit { get; set; }
-    public string IdPeg { get; set; }
-    public string Jns_Bend { get; set; }
-    public int StAktif { get; set; }
-    public string IdBank { get; set; }
-    public string Jab_Bend { get; set; }
-    public string RekBend { get; set; }
-    public string NPWPBend { get; set; }
-    public Decimal? SaldoBend { get; set; }
-    public Decimal? SaldoBendT { get; set; }
-    public DateTime? TglStopBend { get; set; }
-    public DateTime? DateCreate { get; set; }
+      private readonly IMapper _mapper;
+
+      public string IdUnit { get; set; }
+      public string IdPeg { get; set; }
+      public string Jns_Bend { get; set; }
+      public int StAktif { get; set; }
+      public string IdBank { get; set; }
+      public string Jab_Bend { get; set; }
+      public string RekBend { get; set; }
+      public string NPWPBend { get; set; }
+      public Decimal? SaldoBend { get; set; }
+      public Decimal? SaldoBendT { get; set; }
+      public DateTime? TglStopBend { get; set; }
+      public DateTime? DateCreate { get; set; }
+
+      public DTO()
+      {
+        var config = new MapperConfiguration(opt =>
+        {
+          opt.CreateMap<DTO, Command>();
+        });
+
+        _mapper = config.CreateMapper();
+      }
+
+      public Command MapDTO(Command destination)
+      {
+        return _mapper.Map(this, destination);
+      }
     }
 
-    public class Validator : AbstractValidator<Command>
+    public class Validator : AbstractValidator<DTO>
     {
       public Validator()
       {
-        RuleFor(d => d.IdBend).NotEmpty();
         RuleFor(d => d.IdPeg).NotEmpty();
         RuleFor(d => d.Jns_Bend).NotEmpty();
         RuleFor(d => d.RekBend).NotEmpty();
         RuleFor(d => d.IdBank).NotEmpty();
         RuleFor(d => d.Jab_Bend).NotEmpty();
       }
+    }
+
+    public class Command : IRequest
+    {
+      public string IdBend { get; set; }
+      public string IdUnit { get; set; }
+      public string IdPeg { get; set; }
+      public string Jns_Bend { get; set; }
+      public int StAktif { get; set; }
+      public string IdBank { get; set; }
+      public string Jab_Bend { get; set; }
+      public string RekBend { get; set; }
+      public string NPWPBend { get; set; }
+      public Decimal? SaldoBend { get; set; }
+      public Decimal? SaldoBendT { get; set; }
+      public DateTime? TglStopBend { get; set; }
+      public DateTime? DateCreate { get; set; }
     }
 
     public class Handler : IRequestHandler<Command>
