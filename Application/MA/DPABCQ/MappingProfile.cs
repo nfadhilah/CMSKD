a90afff52457373b80b09099c5bcc102050806cc
Fatah Ramadhan <fatahramadhan1992@gmail.com>
2020-06-19 15:47:29 +0700

    DPAB

---
+++ Application/MA/DPABCQ/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.MA;
+
+namespace Application.MA.DPABCQ
+{
+  public class MappingProfile : Profile
+  {
+    public MappingProfile()
+    {
+      CreateMap<Create.Command, DPAB>();
+      CreateMap<Update.Command, DPAB>();
+    }
+  }
+}
--- Persistence/DbContext.cs
+++ Persistence/DbContext.cs
@@ -30,6 +30,7 @@ namespace Persistence
     PegawaiRepository Pegawai { get; }
     PPKRepository PPK { get; }
     DPARepository DPA { get; }
+    DPABRepository DPAB { get; }
     DaftRekeningRepository DaftRekening { get; }
     PajakRepository Pajak { get; }
     DaftUnitRepository DaftUnit { get; }
@@ -85,6 +86,7 @@ namespace Persistence
     public PegawaiRepository Pegawai => new PegawaiRepository(Connection);
     public PPKRepository PPK => new PPKRepository(Connection);
     public DPARepository DPA => new DPARepository(Connection);
+    public DPABRepository DPAB => new DPABRepository(Connection);
     public DaftRekeningRepository DaftRekening => new DaftRekeningRepository(Connection);
     public PajakRepository Pajak => new PajakRepository(Connection);
     public TahunRepository Tahun => new TahunRepository(Connection);
