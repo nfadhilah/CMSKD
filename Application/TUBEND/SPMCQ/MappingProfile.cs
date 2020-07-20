50da3548486fbd81b3eb06dcd559ca28408a24e2
Mono Noor Fadhilah <halilintar@gmail.com>
2020-07-20 15:43:27 +0800

    Add nospp map on SPMDTO Mapping Profile

--- Application/TUBEND/SPMCQ/MappingProfile.cs
+++ Application/TUBEND/SPMCQ/MappingProfile.cs
@@ -13,6 +13,7 @@ namespace Application.TUBEND.SPMCQ
         .ForMember(d => d.KdUnit, opt => opt.MapFrom(s => s.Unit.KdUnit.Trim()))
         .ForMember(d => d.NmUnit, opt => opt.MapFrom(s => s.Unit.NmUnit.Trim()))
         .ForMember(d => d.NoSPD, opt => opt.MapFrom(s => s.SPD.NoSPD.Trim()))
+        .ForMember(d => d.NoSPP, opt => opt.MapFrom(s => s.SPP.NoSPP.Trim()))
         .ForMember(d => d.TglSPD, opt => opt.MapFrom(s => s.SPD.TglSPD))
         .ForMember(d => d.NmPhk3, opt => opt.MapFrom(s => s.Phk3.NmPhk3.Trim()))
         .ForMember(d => d.NoKontrak, opt => opt.MapFrom(s => s.Kontrak.NoKontrak.Trim()))
