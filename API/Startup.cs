c5a5eb468474a51e29eb34476fc2276e54c56f7b
Mono Noor Fadhilah <halilintar@gmail.com>
2020-12-15 15:00:32 +0800

    Enable Fallback Controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -215,7 +215,7 @@ namespace API
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
-        // endpoints.MapFallbackToController("Index", "Fallback");
+        endpoints.MapFallbackToController("Index", "Fallback");
       });
     }
   }
